fea1776344d0c63b45c6e7b70be8cafd1b0c7b6f
Horchas <adetoyeseolamide48@gmail.com>
2025-01-30 13:13:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,103 @@
+// let myName = "olamide"
+// let myTutor = 'Mr Hakeem'
+// let myWork = 'BAckend programming'
+// prompt("my name  is" +  " " + myName + " " + "my lecturer name is " + " "  + "" + myTutor + " " + "his teaching "+ myWork)
+
+let para = "BRIEF HISTORY OF COMPUTER"
+// alert(para.slice);
+// alert(para.length)
+// alert(para.split)
+
+    // const  myArray =  para.split(' ')
+//     }
+// ))
+// alert(para.toLocaleLowerCase())
+// alert (para.slice(5,14).toLocaleLowerCase())
+// alert(myArray[2])
+
+// let a = 30
+// let b = 10
+// let c = 5
+// let d = 15
+// let e = 2
+//  alert(a*(b-c)+d/e%d)
+ 
+//  alert(a+d)
+//  alert(a-e)
+//  alert(b*c)
+//  alert(c%e)
+//  alert(a/b)
+
+// function greet (midehbrand) {
+//     alert('midehbrand');
+    
+// }
+// function name(params) {
+    
+// }
+
+// funtion types
+//function decleartion
+function greet(olamide) {
+    console.log(`hello, ${olamide}!`);
+};
+
+//function Expression
+const sayHi = function (name) {
+    console.log(`Hello, ${name}`);
+    
+}
+
+//Arrow function
+const Hey = (mideh) => {
+    console.log(`hello, ${mideh}!`);
+    
+}
+
+//function parameters
+function add(a,b){
+    return a+b;
+}
+
+//function return value
+
+function minus (a,b){
+    return a-b;
+}
+
+//function scope
+// function greet(barrytech) {
+//     let message = `hello, ${barrytech} !` ;
+//     console.log(message);
+    
+// }
+
+// function buy (Name, price, amount){
+//    return(`i bought a item ${Name} rice, with the ${price} of 70k, out of the ${amount} 100k, your balance will be 30k.`)
+// }
+
+// function buy(foodName, price, amount) {
+//     const many = Math.floor(amount / price);
+//     const balance = (amount % price)
+
+//     alert(`you can buy ${many} bags of rice and the balance is $${balance}.`);
+// }
+
+// const foodName = 'rice'
+// const price = 70;
+// const amount = 1000;
+
+// console.log(buy("foodName",price,amount));
+
+
+
+
+
+
+
+
+
+
+
+
+
